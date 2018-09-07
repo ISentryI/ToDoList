62eecaa1ec448b5b0c143897a92dedcb384d92b7
Eric Martinez Paredes <aiacos_eric@hotmail.com>
2018-09-06 23:09:06 -0500

    part three

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,28 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Eric Martínez Paredes on 03/09/18.
+//  Copyright © 2018 ACL. All rights reserved.
+//
+
+import UIKit
+
+struct ToDo {
+    var dueDate : Date
+    var title : String
+    var isComplete : Bool
+    var notes : String?
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(dueDate: Date(), title: "ToDo 1", isComplete: false, notes: "Nota 1")
+        let todo2 = ToDo(dueDate: Date(), title: "ToDo 2", isComplete: false, notes: "Nota 2")
+        let todo3 = ToDo(dueDate: Date(), title: "ToDo 3", isComplete: false, notes: "Nota 3")
+        
+        return [todo1, todo2, todo3]
+    }
+}
